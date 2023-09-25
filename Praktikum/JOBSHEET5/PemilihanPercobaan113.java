6d7839e08d89c3e7078fb2370c185a7e96c19233
GabrielBatavia <gabrielbatavia7@gmail.com>
2023-09-25 14:27:52 +0700

    Menambahkan file PemilihanPercobaan113.java

---
+++ Praktikum/JOBSHEET5/PemilihanPercobaan113.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+/**
+ * PemilihanPercobaan113
+ */
+public class PemilihanPercobaan113 {
+
+    public static void main(String[] args) {
+
+        Scanner input13 = new Scanner(System.in);
+
+        System.out.print("Masukkan angka: ");
+        int angka = input13.nextInt();
+
+        /*
+        if (angka % 2 == 0)
+            System.out.println("Angka "+angka+" bilangan genap");
+        else
+            System.out.println("Angka "+angka+" bilangan ganjil");
+        */
+        
+        String result = (angka % 2 == 0) ? "Angka "+angka+" bilangan genap" : "Angka "+angka+" bilangan ganjil";
+        System.out.println(result);
+    }
+}
