608341648edead662ced721b42d68d72b51616e5
GabrielBatavia <gabrielbatavia7@gmail.com>
2023-10-02 11:28:33 +0700

    Melakukan modifikasi di else Pemilihan2Percobaan1_13

--- Semester 1/Praktikum/JOBSHEET6/Pemilihan2Percobaan1_13.java
+++ Semester 1/Praktikum/JOBSHEET6/Pemilihan2Percobaan1_13.java
@@ -13,10 +13,12 @@ public class Pemilihan2Percobaan1_13 {
     System.out.print("Masukkan Tahun : ");
     tahun = sc.nextInt();
 
-    if ((tahun % 4) == 0 ) {
-        if ((tahun % 100) != 0) {
+    if ((tahun % 4) == 0 && (tahun % 100) != 0) {
+        /*if ((tahun % 100) != 0) {
             System.out.println("Tahun Kabisat");
-        }
+        } else {
+            System.out.println("Bukan Tahun Kabisat");
+        }*/
     } else {
         System.out.println("Bukan Tahun Kabisat");
     }
