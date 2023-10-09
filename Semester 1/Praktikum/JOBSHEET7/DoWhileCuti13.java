5b3baa4e8f703a61eb1801a55fc615b6cd0aff27
GabrielBatavia <gabrielbatavia7@gmail.com>
2023-10-09 13:21:44 +0700

    Menambahkan file DoWhileCuti13

---
+++ Semester 1/Praktikum/JOBSHEET7/DoWhileCuti13.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+/**
+ * DoWhileCuti
+ */
+public class DoWhileCuti13 {
+
+    public static void main(String[] args) {
+        
+    Scanner sc13 = new Scanner(System.in);
+
+    int jatahCuti, jumlahHari;
+    String konfirmasi;
+
+    System.out.print("Jatah cuti : ");
+    jatahCuti = sc13.nextInt();
+
+    do {
+        System.out.print("Apakah Anda ingin mengambil cuti (y/t)? ");
+        konfirmasi = sc13.next();
+
+        if (konfirmasi.equalsIgnoreCase("y")) {
+            System.out.print("Jumlah hari : ");
+            jumlahHari = sc13.nextInt();
+
+            if (jumlahHari <= jatahCuti) {
+                jatahCuti -= jumlahHari;
+                System.out.println("Sisa jatah cuti: " + jatahCuti);
+            } else {
+                System.out.println("Sisa jatah cuti Anda tidak mencukupi");
+                break;
+            }
+        }
+
+    } while (jatahCuti > 0);
+    
+    }
+}
