dfbbcc04867036c7943d14d13838e89a0a7fbd70
GabrielBatavia <gabrielbatavia7@gmail.com>
2023-10-09 10:56:49 +0700

    Menambahkan file WhileKelipatan13

--- Semester 1/Praktikum/JOBSHEET7/ForKelipatan13.java
+++ Semester 1/Praktikum/JOBSHEET7/ForKelipatan13.java
@@ -20,9 +20,11 @@ public class ForKelipatan13 {
         }
     }
 
+    double rataRata = (double) jumlah / counter;
+
     System.out.printf("Banyaknya bilangan %d dari 1 sampai 50 adalah %d\n", kelipatan, counter);
     System.out.printf("Total bilangan kelipatan %d dari 1 sampai 50 adalah %d\n", kelipatan, jumlah);
-
+    System.out.printf("Rata-rata bilangan kelipatan %d dari 1 sampai 50 adalah %.2f\n", kelipatan, rataRata);
 
     }
 }
